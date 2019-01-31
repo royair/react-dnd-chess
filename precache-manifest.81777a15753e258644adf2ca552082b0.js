8b4b769327faef3f6ade686610f0ad2b54c3a2c8
roy yair <roy.yair@gmail.com>
2019-02-01 00:42:15 +0200

    Updates

---
+++ precache-manifest.81777a15753e258644adf2ca552082b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6b342185b2e2a67afd5",
+    "url": "/react-dnd-chess/static/js/runtime~main.f6b34218.js"
+  },
+  {
+    "revision": "4ee9217825cde7b1f593",
+    "url": "/react-dnd-chess/static/js/main.4ee92178.chunk.js"
+  },
+  {
+    "revision": "a708982b8c1d625f2309",
+    "url": "/react-dnd-chess/static/js/1.a708982b.chunk.js"
+  },
+  {
+    "revision": "4ee9217825cde7b1f593",
+    "url": "/react-dnd-chess/static/css/main.ecd3ce48.chunk.css"
+  },
+  {
+    "revision": "83c2420b4a67ee57e6aa8cb2061de7a9",
+    "url": "/react-dnd-chess/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-dnd-chess/precache-manifest.81777a15753e258644adf2ca552082b0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-dnd-chess/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
